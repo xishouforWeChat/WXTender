82ef480561d69248e5b972d21815fe904ae0c775
1126978661@qq.com <1126978661@qq.com>
2017-04-17 14:01:42 +0800

    [descriptions] 1,添加自动回复

--- handle.py
+++ handle.py
@@ -3,6 +3,8 @@
 # ea8dcc17879d52f8dc1290deb581cf65
 import hashlib
 import web
+import reply
+import receive
 
 class Handle(object):
     def GET(self):
@@ -28,3 +30,20 @@ class Handle(object):
                 return ""
         except Exception, Argument:
             return Argument
+
+    def POST(self):
+        try:
+            webData = web.data()
+            print "Handle Post webdata is ", webData   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
+            else:
+                print "暂且不处理"
+                return "success"
+        except Exception, Argment:
+            return Argment
