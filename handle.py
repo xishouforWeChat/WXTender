c86562384c9e8ac23ed79e5f53c9dd8718f65c2b
1126978661@qq.com <1126978661@qq.com>
2017-04-17 14:20:13 +0800

    [descriptions] 1,添加图片和文字自动回复

--- handle.py
+++ handle.py
@@ -36,14 +36,21 @@ class Handle(object):
             webData = web.data()
             print "Handle Post webdata is ", webData   #后台打日志
             recMsg = receive.parse_xml(webData)
-            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+            if isinstance(recMsg, receive.Msg):
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = "test"
-                replyMsg = reply.TextMsg(toUser, fromUser, content)
-                return replyMsg.send()
+                if recMsg.MsgType == 'text':
+                    content = "test"
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                    return replyMsg.send()
+                if recMsg.MsgType == 'image':
+                    mediaId = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+                    return replyMsg.send()
+                else:
+                    return reply.Msg().send()
             else:
                 print "暂且不处理"
-                return "success"
+                return reply.Msg().send()
         except Exception, Argment:
             return Argment
