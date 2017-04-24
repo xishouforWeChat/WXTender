65993b9be110fdd6dd48a8f79921a183294221b8
1126978661@qq.com <1126978661@qq.com>
2017-04-24 15:02:39 +0800

    [descriptions] 1,自定义菜单

---
+++ menu.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# filename: menu.py
+import urllib
+from basic import Basic
+
+
+class Menu(object):
+    def __init__(self):
+        pass
+
+    def create(self, postData, accessToken):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % accessToken
+        if isinstance(postData, unicode):
+            postData = postData.encode('utf-8')
+        urlResp = urllib.urlopen(url=postUrl, data=postData)
+        print urlResp.read()
+
+    def query(self, accessToken):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token=%s" % accessToken
+        urlResp = urllib.urlopen(url=postUrl)
+        print urlResp.read()
+
+    def delete(self, accessToken):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=%s" % accessToken
+        urlResp = urllib.urlopen(url=postUrl)
+        print urlResp.read()
+
+    # 获取自定义菜单配置接口
+    def get_current_selfmenu_info(self, accessToken):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token=%s" % accessToken
+        urlResp = urllib.urlopen(url=postUrl)
+        print urlResp.read()
+
+
+if __name__ == '__main__':
+    myMenu = Menu()
+    postJson = """
+    {
+        "button":
+        [
+            {
+             "type":"miniprogram",
+             "name":"招投标搜索",
+             "url":"http://mp.weixin.qq.com",
+             "appid":"wx3f8eb38c63060bf4",
+             "pagepath":"pages/index/index.html"
+            },
+            {	
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            },
+            {
+                "name": "关于",
+                "sub_button":
+                [
+                    {	
+                       "type":"view",
+                       "name":"关于我们",
+                       "url":"http://www.soso.com/"
+                    },
+                    {
+                        "type": "view",
+                        "name": "使用技巧",
+                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
+                    },
+                    {
+                        "type": "view",
+                        "name": "意见反馈",
+                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1433747234&token=&lang=zh_CN"
+                    }
+                ]
+            }
+          ]
+    }
+    """
+    accessToken = Basic().get_access_token()
+    myMenu.create(postJson, accessToken)
